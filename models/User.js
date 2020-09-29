042242e20a3b5b6b739cc8b35b80fda03f5d4661
Mindaugas <strumke@yahoo.com>
2020-09-29 20:26:28 +0200

    user shema added

--- models/User.js
+++ models/User.js
@@ -1,30 +1,14 @@
-const mongoose = require("mongoose");
-//const uniqueValidator = require("mongoose-unique-validator");
+import mongoose from "mongoose";
 
+/* PetSchema will correspond to a collection in your MongoDB database. */
 const UserSchema = new mongoose.Schema({
   name: {
-    type: String,
-    required: [true, "Name is required"],
-    minlength: [4, "Min length is 4"],
-  },
-  // email: {
-  //   type: String,
-  //   pattern: "^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}$",
-  //   description: "must be a string and match the regular expression pattern",
-  //   unique: true,
-  // },
-  // passwordHash: {
-  //   type: String,
-  //   minlength: 3,
-  //   required: true,
-  // },
-  // videoNotes: [
-  //   {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: "VideoNote",
-  //   },
-  // ],
-});
-//userSchema.plugin(uniqueValidator);
+    /* The name of this pet */
 
-module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
+    type: String,
+    required: [true, "Please provide a name for this pet."],
+    maxlength: [20, "Name cannot be more than 60 characters"],
+  },
+});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
